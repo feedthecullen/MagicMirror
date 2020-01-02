88b27a9f4317621cb64552642c7c7eb76080d669
Brenton <6r3nt0n@gmail.com>
2020-01-01 20:19:58 -0600

    Add files via upload

---
+++ modules/MMM-Launch/MMM-Launch.js
@@ -0,0 +1,167 @@
+/* Magic Mirror
+ * Module: MMM-Launch
+ *
+ * By Mykle1
+ *
+ */
+ 
+Module.register("MMM-Launch", {
+
+    // Module config defaults.
+    defaults: {
+        showPix: "Yes",                 // No = no pictures display
+	showAgency: "Yes",              // No = Launch Agency not shown
+	showDescription: "No",          // Yes for full descriptive text under picture
+        useHeader: true,                // false if you don't want a header
+        header: "We have liftoff!",     // Any text you want
+        maxWidth: "250px",
+        rotateInterval: 30 * 1000,      // 30 seconds
+        animationSpeed: 3000,           // fade in and out speed
+        initialLoadDelay: 5250,
+        retryDelay: 2500,
+        updateInterval: 30 * 60 * 1000, // 60 lauches per call
+
+    },
+
+    getStyles: function() {
+        return ["MMM-Launch.css"];
+    },
+
+    start: function() {
+        Log.info("Starting module: " + this.name);
+
+        requiresVersion: "2.1.0",
+
+        // Set locale.
+        this.url = "https://launchlibrary.net/1.4/launch?next=5&mode=verbose";
+        this.Launch = [];
+        this.activeItem = 0;
+        this.rotateInterval = null;
+        this.scheduleUpdate();
+    },
+
+    getDom: function() {
+
+        var wrapper = document.createElement("div");
+        wrapper.className = "wrapper";
+        wrapper.style.maxWidth = this.config.maxWidth;
+
+        if (!this.loaded) {
+            wrapper.innerHTML = "T - Minus 5 seconds and counting";
+            wrapper.classList.add("bright", "light", "small");
+            return wrapper;
+        }
+
+        if (this.config.useHeader != false) {
+            var header = document.createElement("header");
+            header.classList.add("xsmall", "bright", "light", "header");
+            header.innerHTML = this.config.header;
+            wrapper.appendChild(header);
+        }
+
+
+			var LaunchKeys = Object.keys(this.Launch);
+        if (LaunchKeys.length > 0) {
+            if (this.activeItem >= LaunchKeys.length) {
+                this.activeItem = 0;
+            }
+            var Launch = this.Launch[LaunchKeys[this.activeItem]];
+
+
+			var top = document.createElement("div");
+			top.classList.add("list-row");
+			
+			// spacecraft // mission type // date of launch // launch site
+			var spacecraft = document.createElement("div");
+			spacecraft.classList.add("xsmall", "bright", "spacecraft");
+			spacecraft.innerHTML = Launch.missions[0].name + " spacecraft for " + Launch.missions[0].typeName + " launches " + this.sTrim(Launch.net, 15, ' ', ' ') + " from " + Launch.location.name;
+			wrapper.appendChild(spacecraft);
+			
+			
+			var showPix = this.config.showPix
+			// picture of rocket type
+			var img = document.createElement("img");
+			img.classList.add("photo");
+			// change placeholder image
+		if (Launch.rocket.imageURL == "https://s3.amazonaws.com/launchlibrary/RocketImages/placeholder_1920.png") {
+			Launch.rocket.imageURL = "https://s3.amazonaws.com/launchlibrary/RocketImages/+Briz-KM_480.jpg";
+		}
+		    // config option for pictures
+		if (this.config.showPix == "Yes") {
+			img.src = Launch.rocket.imageURL; // Launch.rocket.imageURL;
+			wrapper.appendChild(img);
+		}
+		
+			var showDescription = this.config.showDescription
+			// description of mission	
+			var description = document.createElement("div");
+			description.classList.add("xsmall", "bright", "description");
+			// config option for description
+		if (this.config.showDescription == "Yes") {
+			description.innerHTML = Launch.missions[0].description;
+			wrapper.appendChild(description);
+		}
+
+			var showAgency = this.config.showAgency
+			// agencies
+			var agencies = document.createElement("div");
+			agencies.classList.add("xsmall", "bright", "agencies");
+			// config option for Agency
+		if (this.config.showAgency == "Yes") {
+			agencies.innerHTML = "Agency: " + Launch.rocket.agencies[0].name;
+			wrapper.appendChild(agencies);
+			}
+			
+		
+        }
+        return wrapper;
+    },
+
+
+    processLaunch: function(data) {
+        this.today = data.Today;
+        this.Launch = data;
+    //  console.log(this.Launch); // checking my data
+        this.loaded = true;
+    },
+	
+	sTrim: function(str, length, delim, appendix) {
+        if (str.length <= length) return str;
+        var trimmedStr = str.substr(0, length + delim.length);
+        var lastDelimIndex = trimmedStr.lastIndexOf(delim);
+        if (lastDelimIndex >= 0) trimmedStr = trimmedStr.substr(0, lastDelimIndex);
+        if (trimmedStr) trimmedStr += appendix;
+        return trimmedStr;
+    },
+
+    scheduleCarousel: function() {
+        console.log("Carousel of Launch fucktion!");
+        this.rotateInterval = setInterval(() => {
+            this.activeItem++;
+            this.updateDom(this.config.animationSpeed);
+        }, this.config.rotateInterval);
+    },
+
+    scheduleUpdate: function() {
+        setInterval(() => {
+            this.getLaunch();
+        }, this.config.updateInterval);
+        this.getLaunch(this.config.initialLoadDelay);
+        var self = this;
+    },
+
+    getLaunch: function() {
+        this.sendSocketNotification('GET_LAUNCH', this.url);
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "LAUNCH_RESULT") {
+            this.processLaunch(payload);
+            if (this.rotateInterval == null) {
+                this.scheduleCarousel();
+            }
+            this.updateDom(this.config.animationSpeed);
+        }
+        this.updateDom(this.config.initialLoadDelay);
+    },
+});
---
+++ modules/MMM-json-feed/README.md
@@ -0,0 +1,90 @@
+# MMM-json-feed
+A MagicMirror module that displays information from any json source
+
+![status](https://img.shields.io/badge/status-not%20maintained-red.svg)
+
+This code is partially copied from the very simple [stock ticker](https://github.com/alexyak/stocks) from [_@alexyak_](https://github.com/alexyak).
+
+__Notice__: I have no intention of adding new features to the code here. Multiple people have asked for more customized features. If you are interested in something cool and awesome that isn't implemented, let me know and if I do get time I may be able to help. A small donation to the beer/coffee fund never hurts (paypal: amcolash@gmail.com) to show your appreciation (by no means required)! I love open source software and love providing time to my projects, but I don't use this module so it is not high on my list of things to do. As always, if you make modifications start a PR and we can merge it into the master branch.
+
+## What it Looks Like!
+Nothing too pretty, just the data you want to see :)
+
+There are some config options to make the json keys a bit prettier though, mentioned below in the config.
+
+![Screenshot](https://raw.githubusercontent.com/amcolash/MMM-json-feed/master/Screenshot.png)
+
+Here is the above json example data:
+```
+{
+  "andrewMcolash":true,
+  "AndrewMc":"123",
+  "test_this":["a","b","c"],
+  "Test_This":{"e":true,"f":"abc"}
+}
+```
+
+## Install
+`cd MagicMirror/modules`
+`git clone https://github.com/amcolash/MMM-json-feed.git`
+
+## Update
+`cd MagicMirror/modules/MMM-json-feed`
+`git pull`
+
+## Configuration
+It is very simple to set up this module, a sample configuration looks like this:
+
+```
+modules: [
+  {
+    module: 'MMM-json-feed',
+    position: 'bottom_bar',
+    config: {
+      urls: [
+        'http://your.server.json.here/abc1.json',
+        'http://your.server.json.here/abc2.json',
+      ]
+    }
+  }
+]
+```
+
+## Configuration Options
+
+| Option               | Description
+| -------------------- | -----------
+| `prettyName`         | Pretty print the name of each JSON key (remove camelCase and underscores). <br><br> **Default value:** `true`
+| `stripName`          | Removes all keys before the printed key. <br><br>**Example:** `a.b.c` will print `c`.<br> **Default value:** `true`
+| `title`              | Title to display at the top of the module. <br><br> **Default value:** `JSON Feed`
+| `urls`               | An array of urls for your json feeds. Note that duplicate keys *WILL* be clobbered.<br><br> **Default value:** `REQUIRED`
+| `url`                | **DEPRECATED, Please use `urls` instead.**<br>~~The url of the json feed. <br> **Default value:** `REQUIRED`~~
+| `updateInterval`     | The time between updates (In milliseconds). <br><br> **Default value:** `300000 (5 minutes)`
+| `singleLine`         | Display all values on a single line.<br><br> **Default value:** `false`
+| `values`             | Specify specific values from the json feed to only show what you need. <br><br>**Example:** `["key1", "key2", "keyA.keyB.keyC"]`<br> **Default value:** `[]` (Shows all keys in the object)
+| `arrayName`          | Name of array of items to iterate through.<br><br> **Default value:** `undefined`
+| `arraySize`          | Number of array of items to show.<br><br> **Default value:** `999`
+| `replaceName`        | Specify key names to replace in the json. This is an array of arrays [find, replace]<br><br>**Example:** `[ ["body", "replaced body"], ["id", "replacedID"] ]`<br>
+
+## Using an Array of Data and Custom Parsing
+You can use the new config `arrayName` to parse through an array and then add parts from each object.
+
+For the given json:
+```
+{
+  messages: [
+    { name: "a", id: 1, somethingElse: false },
+    { name: "b", id: 2, somethingElse: false },
+    { name: "c", id: 3, somethingElse: true }
+  ]
+}
+```
+you could have in your config (to show only the parts you care about and limit to 5 entries):
+```
+config: {
+  ...
+  arrayName: "messages",
+  arraySize: 5,
+  values: [ "name", "id" ]
+}
+```
